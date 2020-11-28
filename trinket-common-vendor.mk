14110b7674966ea6d53915c66e1b98b0c16b8017
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-11-28 15:57:18 +0530

    trinket-common: Switch to OSS libplatformconfig

--- trinket-common-vendor.mk
+++ trinket-common-vendor.mk
@@ -812,7 +812,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libantradio \
     libMpeg4SwEncoder \
-    libplatformconfig \
     vendor.qti.hardware.fm@1.0 \
     CneApp \
     IWlanService \
